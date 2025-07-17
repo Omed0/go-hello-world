a18098e103ddc82397d3ea5a11d95ec26fcd7617
omed0 <omedakram54@gmail.com>
2025-07-17 04:12:29 +0000

    change somethings

--- main.go
+++ main.go
@@ -7,7 +7,8 @@ func main() {
 	nameProgram := "Go"
 
 	fmt.Printf("Welcome to the %v program!\n", nameProgram) // This program demonstrates basic variable declaration and function usage in Go.
-	fmt.Println(hello)
+	fmt.Printf("Message: %s\n", hello)                      // This line prints the message stored in the variable hello.
+	//fmt.Println(hello)
 
 	var x uint = 10
 	fmt.Println("Value of x:", x)
@@ -16,6 +17,7 @@ func main() {
 	fmt.Println("Value of y:", y)
 
 	fmt.Println("Value of total is:", sum(x, y))
+
 }
 
 func sum(a uint, b int) uint {
